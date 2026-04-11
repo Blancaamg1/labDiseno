80f2810462bbefc40e694c4fda1c576411eab1c0
Blancaamg1 <blanca.alanon@alu.uclm.es>
2026-04-11 13:31:02 +0200

    info compra

---
+++ esientradas/src/main/java/edu/esi/ds/esientradas/dto/DtoCompraInfo.java
@@ -0,0 +1,50 @@
+package edu.esi.ds.esientradas.dto;
+
+public class DtoCompraInfo {
+    private long idEspectaculo;
+    private String nombreEspectaculo;
+    private String tipoMapa;
+    private String nombreEscenario;
+    private String modoSeleccion;
+    private long idEscenario;
+
+    
+    public long getIdEspectaculo() {
+        return idEspectaculo;
+    }
+    public void setIdEspectaculo(long idEspectaculo) {
+        this.idEspectaculo = idEspectaculo;
+    }
+    public String getNombreEspectaculo() {
+        return nombreEspectaculo;
+    }
+    public void setNombreEspectaculo(String nombreEspectaculo) {
+        this.nombreEspectaculo = nombreEspectaculo;
+    }
+    public String getTipoMapa() {
+        return tipoMapa;
+    }
+    public void setTipoMapa(String tipoMapa) {
+        this.tipoMapa = tipoMapa;
+    }
+    public String getNombreEscenario() {
+        return nombreEscenario;
+    }
+    public void setNombreEscenario(String nombreEscenario) {
+        this.nombreEscenario = nombreEscenario;
+    }
+    public String getModoSeleccion() {
+        return modoSeleccion;
+    }
+    public void setModoSeleccion(String modoSeleccion) {
+        this.modoSeleccion = modoSeleccion;
+    }
+    public long getIdEscenario() {
+        return idEscenario;
+    }
+    public void setIdEscenario(long idEscenario) {
+        this.idEscenario = idEscenario;
+    }
+
+    
+}
